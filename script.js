22e3dd9450ca5b5974d4349131d72bc805336fd2
EricLiu27 <eric.liu82794@gmail.com>
2023-06-20 00:27:51 -0400

    Liking Likes

---
+++ script.js
@@ -0,0 +1,51 @@
+//PART 1
+
+
+// var count = 3
+// var counter = document.querySelector("#count")
+
+// console.log(countElement);
+
+// function add1() {
+//     count++
+//     counter.innerText = count + " Likes"
+//     console.log(count)
+// }
+
+//PART 2
+
+//ADD1
+var count1 = 9
+var counter1 = document.querySelector("#count1")
+
+
+
+function add1() {
+    count1++
+    counter1.innerText = count1 + " Likes"
+    console.log(count1)
+}
+
+//ADD2
+var count2 = 12
+var counter2 = document.querySelector("#count2")
+
+
+
+function add2() {
+    count2++
+    counter2.innerText = count2 + " Likes"
+    console.log(count2)
+}
+
+//ADD3
+var count3 = 9
+var counter3 = document.querySelector("#count3")
+
+
+
+function add3() {
+    count3++
+    counter3.innerText = count3 + " Likes"
+    console.log(count3)
+}
